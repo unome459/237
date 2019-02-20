a56af36024dc49346c367ac21db9c1f7e0592501
unome459 <carpenterjameson@gmail.com>
2019-02-20 17:54:35 -0500

    first commit

--- head.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <title>Movies</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="webpage.css">
-</head>
--- navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="header">
-    <h1>Movie Log</h1>
-</div>
-<div class="topnav">
-    <a href="index.php">Add Movies</a>
-    <a href="movieslist.php">Movies Watched</a>
-</div>
